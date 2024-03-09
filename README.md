e804ba4f132bef886e3fdd10579f755f7006a34d
Luiz Krol <luizeduardokrolpereira@gmail.com>
2024-03-09 19:56:37 -0300

    dumb

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-:p
+im pretending to put some rust WASM in here and use it as portfolio idk
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav className="text-center font-DelaGothic cursor-default select-none h-10">
+        <h1 className=" movinn">
+          ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ
+          zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz
+          ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ zzz ZZZ
+          zzz ZZZ zzz ZZZ zzz
+        </h1>
+      </nav>
+    </>
+  );
 }
 
 export default App;
